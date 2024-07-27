3941c89c333bcf85f8b7b552c2f2fe78d5852e87
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-07-27 13:50:07 +0100

    Add dbo NOSUPERUSER after crating pgtap in template1.

--- initdb.sh
+++ initdb.sh
@@ -5,11 +5,14 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-ESQL
     CREATE ROLE dbo WITH INHERIT SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'mysecretpassword';
     COMMENT ON ROLE dbo IS 'Application database object owner.';
     GRANT pg_read_server_files, pg_read_all_settings TO dbo;
-    ALTER ROLE dbo WITH NOSUPERUSER;
 ESQL
 
 psql -v ON_ERROR_STOP=1 --username "dbo" --dbname "template1" <<-ESQL
     CREATE SCHEMA pgtap;
     COMMENT ON SCHEMA pgtap IS 'Home of all things PGTap.';
     CREATE EXTENSION pgtap WITH SCHEMA pgtap;
+ESQL
+
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "template1" <<-ESQL
+    ALTER ROLE dbo NOSUPERUSER;
 ESQL
