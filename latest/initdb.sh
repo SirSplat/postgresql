47f291bde25f52b983f4730bae61d146cb13da71
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-11-22 15:31:29 +0000

    Update action to not execute all dockerfiles builds and update latest initdb.sh to trigger its build

--- latest/initdb.sh
+++ latest/initdb.sh
@@ -5,6 +5,9 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-ESQL
     CREATE ROLE dbo WITH INHERIT SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'mysecretpassword';
     COMMENT ON ROLE dbo IS 'Application database object owner.';
     GRANT pg_read_server_files, pg_read_all_settings TO dbo;
+
+    REVOKE ALL ON DATABASE postgres FROM public;
+    REVOKE ALL ON SCHEMA public FROM public;
 ESQL
 
 psql -v ON_ERROR_STOP=1 --username "dbo" --dbname "template1" <<-ESQL
