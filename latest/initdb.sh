b83da938965534591c7ec6851db362195e715420
Leon Rogers <Leon.Rogers@BlueElephantDBA.co.uk>
2024-12-05 10:46:56 +0000

    Allow dbo role to connect to postgres database

--- latest/initdb.sh
+++ latest/initdb.sh
@@ -6,6 +6,8 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-ESQL
     COMMENT ON ROLE dbo IS 'Application database object owner.';
     GRANT pg_read_server_files, pg_read_all_settings TO dbo;
 
+    GRANT CONNECT ON DATABASE postgres TO dbo;
+    
     REVOKE ALL ON DATABASE postgres FROM public;
     REVOKE ALL ON SCHEMA public FROM public;
 ESQL
--- pgtap/initdb.sh
+++ pgtap/initdb.sh
@@ -5,6 +5,8 @@ psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "postgres" <<-ESQL
     CREATE ROLE dbo WITH INHERIT SUPERUSER CREATEDB CREATEROLE LOGIN ENCRYPTED PASSWORD 'mysecretpassword';
     COMMENT ON ROLE dbo IS 'Application database object owner.';
     GRANT pg_read_server_files, pg_read_all_settings TO dbo;
+
+    GRANT CONNECT ON DATABASE postgres TO dbo;
 ESQL
 
 psql -v ON_ERROR_STOP=1 --username "dbo" --dbname "template1" <<-ESQL
